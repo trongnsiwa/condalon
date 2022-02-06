be23b73a4c4188c3d19d41de5e8f7f35fe52e5ca
Si Trong <trongnsiwa79@gmail.com>
2022-02-06 09:58:36 +0700

    build: add blogs part

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Card from "@components/Card";
 import SearchBar from "@components/SearchBar";
 import { createClient, Entry } from "contentful";
 import { IHomeContentFields } from "contentful/__generated__/types";
@@ -10,23 +11,23 @@ const client = createClient({
 });
 
 const Home = () => {
-  const [homeContent, setHomeContent] = useState<Entry<IHomeContentFields>[]>();
+  // const [homeContent, setHomeContent] = useState<Entry<IHomeContentFields>[]>();
 
-  const getHomeContent = async () => {
-    const { items } = await client.getEntries<IHomeContentFields>({
-      content_type: "homeContent"
-    });
+  // const getHomeContent = async () => {
+  //   const { items } = await client.getEntries<IHomeContentFields>({
+  //     content_type: "homeContent"
+  //   });
 
-    setHomeContent(items);
-  };
+  //   setHomeContent(items);
+  // };
 
-  const getFields = (name: string) => {
-    return homeContent?.find((h) => h.fields.name === name).fields;
-  };
+  // const getFields = (name: string) => {
+  //   return homeContent?.find((h) => h.fields.name === name).fields;
+  // };
 
-  useEffect(() => {
-    getHomeContent();
-  }, []);
+  // useEffect(() => {
+  //   getHomeContent();
+  // }, []);
 
   return (
     <>
@@ -34,8 +35,8 @@ const Home = () => {
         {/* hero */}
         <div className="home-hero">
           <div className="home-hero_content">
-            <h1>{getFields("hero")?.title}</h1>
-            <p>{getFields("hero")?.description}</p>
+            <h1>{/* {getFields("hero")?.title} */}</h1>
+            <p>{/* {getFields("hero")?.description} */}</p>
             <a href="#blogs">
               <button className="btn-app">Khám Phá Ngay</button>
             </a>
@@ -44,11 +45,52 @@ const Home = () => {
         {/* blogs */}
         <div id="blogs" className="home-blogs">
           <div className="home-blogs_inner">
-            <h1>{getFields("blogs")?.title}</h1>
+            <h1>
+              {/* {getFields("blogs")?.title} */}
+              Chúng Mình Có Nhiều Blog Hay Dành Cho Bạn
+            </h1>
+            <div className="home-blogs_inner-cards">
+              <Card
+                title="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean."
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem faucibus eget natoque condimentum blandit penatibus posuere amet. Neque purus elit in purus, erat sed. Magna mauris nunc augue ut enim."
+                image="https://images.unsplash.com/photo-1575926994241-013c0af1513e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              />
+              <Card
+                title="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean."
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem faucibus eget natoque condimentum blandit penatibus posuere amet. Neque purus elit in purus, erat sed. Magna mauris nunc augue ut enim."
+                image="https://images.unsplash.com/photo-1525828676209-855204720969?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              />
+              <Card
+                title="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean."
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem faucibus eget natoque condimentum blandit penatibus posuere amet. Neque purus elit in purus, erat sed. Magna mauris nunc augue ut enim."
+                image="https://images.unsplash.com/photo-1589386417686-0d34b5903d23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              />
+            </div>
           </div>
         </div>
         {/* desc */}
-        <div className="home-desc"></div>
+        <div className="home-desc">
+          <div className="home-desc_left">
+            <h2>Bố Mẹ Ơi, Con Đã Lớn</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cum pulvinar pellentesque
+              sapien nulla. Sit blandit elit molestie ut risus dui lorem. In in purus nulla nisi
+              commodo ullamcorper sed turpis. Nullam interdum tempus magna eget sed ac. Nisl purus
+              arcu turpis blandit massa fermentum neque, quam. Erat sit etiam pharetra amet,
+              phasellus cursus auctor. Amet morbi risus nunc, morbi amet id accumsan. Sagittis
+              montes, massa urna, magna sem ac etiam aliquam est. Laoreet sed at venenatis nulla
+              viverra. Sit sed egestas feugiat porta nunc. Vitae in integer tempus pretium tristique
+              lacus pellentesque volutpat facilisis. Quis odio nec volutpat, nisl malesuada neque,
+              quam morbi fringilla. Egestas viverra malesuada facilisis pulvinar aenean eget ipsum
+              erat. Aenean pellentesque lorem lorem vulputate tincidunt sapien. Tempus elit mattis
+              auctor at amet.
+            </p>
+            <button className="btn-app">Xem thêm</button>
+          </div>
+          <div className="home-desc_right">
+            <img src="" alt="" />
+          </div>
+        </div>
         {/* desc2 */}
         <div className="home-desc2"></div>
         {/* hero2 */}
