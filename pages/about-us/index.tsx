ae4f86e7e4fe310bc9145379f71af7db035728eb
Phuc <phucldh.work@gmail.com>
2022-02-07 10:18:48 +0700

    Done about us page

--- pages/about-us/index.tsx
+++ pages/about-us/index.tsx
@@ -1,78 +1,63 @@
+import Profile from "@components/Profile";
+
 const AboutUs = () => {
-  
-    return (
+  return (
     <>
       <div className="about">
         {/* hero */}
         <div className="about-hero"></div>
         <div className="text-center">
-          <h1 className="about-content">
-            Chúng tôi là ai
-          </h1>
-          <h4 className="about-subcontent">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pellentesque mattis tortor vestibulum, leo non. Pharetra arcu egestas elit cras. Enim sollicitudin eleifend nibh tincidunt. Vulputate laoreet tempus id lacus quis sit id enim, tincidunt. Mattis mauris eu dui et nibh facilisis maecenas. Massa maecenas facilisis etiam est egestas neque.
-            Dolor lacus, sed sit nulla. In nunc lobortis magna sed bibendum. Enim proin scelerisque a justo aliquet neque tristique.
-          </h4>
+          <h1 className="about-content">Chúng tôi là ai</h1>
+          <h5 className="about-subcontent">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque pellentesque mattis tortor vestibulum, leo non.
+            Pharetra arcu egestas elit cras. Enim sollicitudin eleifend nibh
+            tincidunt. Vulputate laoreet tempus id lacus quis sit id enim,
+            tincidunt. Mattis mauris eu dui et nibh facilisis maecenas. Massa
+            maecenas facilisis etiam est egestas neque. Dolor lacus, sed sit
+            nulla. In nunc lobortis magna sed bibendum. Enim proin scelerisque a
+            justo aliquet neque tristique.
+          </h5>
         </div>
-        <div>
-          <img src="https://scontent-hkg4-1.xx.fbcdn.net/v/t1.6435-9/150187945_3085502935029500_7966127298109183408_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=Hfry0Lk17SsAX9eQ0C0&tn=bBkZQ3dUdZyToqpu&_nc_ht=scontent-hkg4-1.xx&oh=00_AT-FFBstHJGweY3XJGkniVF4uPInU2ficW6SoMhR5K_ctQ&oe=62247DD2" alt="Hoang Phuc Img" width="500" height="600"></img>
-          <h1 className="about-content2">
-            Hoang Phuc
-          </h1>
-          <h4 className="about-subcontent">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pellentesque mattis tortor vestibulum, leo non. Pharetra arcu egestas elit cras. Enim sollicitudin eleifend nibh tincidunt. Vulputate laoreet tempus id lacus quis sit id enim, tincidunt. Mattis mauris eu dui et nibh facilisis maecenas. Massa maecenas facilisis etiam est egestas neque.
-            Dolor lacus, sed sit nulla. In nunc lobortis magna sed bibendum. Enim proin scelerisque a justo aliquet neque tristique.
-          </h4>
-        </div>
-        <div>
-          <img src="https://scontent-hkg4-1.xx.fbcdn.net/v/t1.6435-9/150187945_3085502935029500_7966127298109183408_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=Hfry0Lk17SsAX9eQ0C0&tn=bBkZQ3dUdZyToqpu&_nc_ht=scontent-hkg4-1.xx&oh=00_AT-FFBstHJGweY3XJGkniVF4uPInU2ficW6SoMhR5K_ctQ&oe=62247DD2" alt="Thien Phuc Img" width="500" height="600"></img>
-          <h1 className="about-content2">
-            Thien Phuc
-          </h1>
-          <h4 className="about-subcontent">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pellentesque mattis tortor vestibulum, leo non. Pharetra arcu egestas elit cras. Enim sollicitudin eleifend nibh tincidunt. Vulputate laoreet tempus id lacus quis sit id enim, tincidunt. Mattis mauris eu dui et nibh facilisis maecenas. Massa maecenas facilisis etiam est egestas neque.
-            Dolor lacus, sed sit nulla. In nunc lobortis magna sed bibendum. Enim proin scelerisque a justo aliquet neque tristique.
-          </h4>
-        </div>
-        <div>
-          <img src="https://scontent-hkg4-1.xx.fbcdn.net/v/t39.30808-1/p200x200/272970878_1587874038233353_8729411479677462416_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=7206a8&_nc_ohc=68Bq47sRlUwAX_VA_im&_nc_ht=scontent-hkg4-1.xx&oh=00_AT8rd5oXk7e2JG7TcJwsg0pd68eL49Bqg0dZItoESyT2SQ&oe=620571BE" alt="Si Trong Img" width="500" height="600"></img>
-          <h1 className="about-content2">
-            Si Trong
-          </h1>
-          <h4 className="about-subcontent">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pellentesque mattis tortor vestibulum, leo non. Pharetra arcu egestas elit cras. Enim sollicitudin eleifend nibh tincidunt. Vulputate laoreet tempus id lacus quis sit id enim, tincidunt. Mattis mauris eu dui et nibh facilisis maecenas. Massa maecenas facilisis etiam est egestas neque.
-            Dolor lacus, sed sit nulla. In nunc lobortis magna sed bibendum. Enim proin scelerisque a justo aliquet neque tristique.
-          </h4>
-        </div>
-        <div>
-          <img src="https://scontent-hkg4-1.xx.fbcdn.net/v/t1.6435-9/69688823_2405896056401331_4300693621276934144_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=UiszCtKIhaoAX-VcK1H&_nc_oc=AQl5-lriQ_s27T7OQlDpvq_17qorz4GVaq-tK6gaaXPbhClAgS2NBF3Y06XHQqglU4k&tn=bBkZQ3dUdZyToqpu&_nc_ht=scontent-hkg4-1.xx&oh=00_AT9SEALZlkdZbudEEK968GyBNEcwSPFYWtWMg-r_ZABG5A&oe=622619BB" alt="Quyen Duc Img" width="500" height="600"></img>
-          <h1 className="about-content2">
-            Quyen Duc
-          </h1>
-          <h4 className="about-subcontent">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pellentesque mattis tortor vestibulum, leo non. Pharetra arcu egestas elit cras. Enim sollicitudin eleifend nibh tincidunt. Vulputate laoreet tempus id lacus quis sit id enim, tincidunt. Mattis mauris eu dui et nibh facilisis maecenas. Massa maecenas facilisis etiam est egestas neque.
-            Dolor lacus, sed sit nulla. In nunc lobortis magna sed bibendum. Enim proin scelerisque a justo aliquet neque tristique.
-          </h4>
-        </div>
-        <div>
-          <img src="https://scontent-hkg4-1.xx.fbcdn.net/v/t39.30808-6/273148976_1618130745191674_657353332608960525_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=__Ao6VPcDEUAX9IAyuY&_nc_ht=scontent-hkg4-1.xx&oh=00_AT9sgjhcKPoDPNM16e9rgbDeKWi8yV4aSz-jk2hrHRkLLA&oe=62047866" alt="Quang Dat Img" width="500" height="600"></img>
-          <h1 className="about-content2">
-            Quang Dat
-          </h1>
-          <h4 className="about-subcontent">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pellentesque mattis tortor vestibulum, leo non. Pharetra arcu egestas elit cras. Enim sollicitudin eleifend nibh tincidunt. Vulputate laoreet tempus id lacus quis sit id enim, tincidunt. Mattis mauris eu dui et nibh facilisis maecenas. Massa maecenas facilisis etiam est egestas neque.
-            Dolor lacus, sed sit nulla. In nunc lobortis magna sed bibendum. Enim proin scelerisque a justo aliquet neque tristique.
-          </h4>
-        </div>
-        <div>
-          <img src="https://scontent-hkg4-1.xx.fbcdn.net/v/t1.6435-9/82323446_2769075446705765_1871776271134258930_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=174925&_nc_ohc=OWbcJZhA6TkAX83WcJb&_nc_ht=scontent-hkg4-1.xx&oh=00_AT9Vu13QN19Iq_fo6EfgySefweSbeNc4ceDT0kfXLQaMBw&oe=62251A06" alt="Viet Bach Img" width="500" height="600"></img>
-          <h1 className="about-content2">
-            Viet Bach
-          </h1>
-          <h4 className="about-subcontent">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pellentesque mattis tortor vestibulum, leo non. Pharetra arcu egestas elit cras. Enim sollicitudin eleifend nibh tincidunt. Vulputate laoreet tempus id lacus quis sit id enim, tincidunt. Mattis mauris eu dui et nibh facilisis maecenas. Massa maecenas facilisis etiam est egestas neque.
-            Dolor lacus, sed sit nulla. In nunc lobortis magna sed bibendum. Enim proin scelerisque a justo aliquet neque tristique.
-          </h4>
+        <div className="about-profile">
+          <div className="about-profile_inner">
+            <div className="about-profile_inner-cards">
+              <Profile
+                name="Hoang Phuc"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem faucibus eget natoque condimentum blandit penatibus posuere amet. Neque purus elit in purus, erat sed. Magna mauris nunc augue ut enim."
+                image="/images/avatar/hoangphuc.jpg"
+              />
+              <Profile
+                name="Thien Phuc"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem faucibus eget natoque condimentum blandit penatibus posuere amet. Neque purus elit in purus, erat sed. Magna mauris nunc augue ut enim."
+                image="/images/avatar/thienphuc.jpg"
+              />
+              <Profile
+                name="Si Trong"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem faucibus eget natoque condimentum blandit penatibus posuere amet. Neque purus elit in purus, erat sed. Magna mauris nunc augue ut enim."
+                image="/images/avatar/sitrong.jpg"
+              />
+            </div>
+          </div>
+          <div className="about-profile_inner">
+            <div className="about-profile_inner-cards">
+              <Profile
+                name="Quyen Duc"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem faucibus eget natoque condimentum blandit penatibus posuere amet. Neque purus elit in purus, erat sed. Magna mauris nunc augue ut enim."
+                image="/images/avatar/quyenduc.jpg"
+              />
+              <Profile
+                name="Quang Dat"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem faucibus eget natoque condimentum blandit penatibus posuere amet. Neque purus elit in purus, erat sed. Magna mauris nunc augue ut enim."
+                image="images/avatar/quangdat.jpg"
+              />
+              <Profile
+                name="Viet Bach"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem faucibus eget natoque condimentum blandit penatibus posuere amet. Neque purus elit in purus, erat sed. Magna mauris nunc augue ut enim."
+                image="images/avatar/vietbach.jpg"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </>
