00f520569ad52116aa7ae4bb65a58fe8b2928ebc
Si Trong <trongnsiwa79@gmail.com>
2022-02-08 15:57:44 +0700

    build: create contact page

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -4,8 +4,11 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import SearchBar from "@components/SearchBar";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function Navbar() {
+  const router = useRouter();
+
   return (
     <BNavbar bg="app-light" expand="lg" className="shadow">
       <Container>
@@ -19,19 +22,19 @@ function Navbar() {
         <BNavbar.Toggle aria-controls="responsive-navbar-nav" />
         <BNavbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Item>
+            <Nav.Item className={router.pathname.split("?")[0] == "/" ? "active" : ""}>
               <Link href="/">Trang chủ</Link>
             </Nav.Item>
-            <Nav.Item>
+            <Nav.Item className={router.pathname.split("?")[0] == "/blog" ? "active" : ""}>
               <Link href="/blog">Blog</Link>
             </Nav.Item>
-            <Nav.Item>
+            <Nav.Item className={router.pathname.split("?")[0] == "/introduce" ? "active" : ""}>
               <Link href="/introduce">Giới thiệu</Link>
             </Nav.Item>
-            <Nav.Item>
+            <Nav.Item className={router.pathname.split("?")[0] == "/about-us" ? "active" : ""}>
               <Link href="/about-us">Chúng tôi là</Link>
             </Nav.Item>
-            <Nav.Item>
+            <Nav.Item className={router.pathname.split("?")[0] == "/contact" ? "active" : ""}>
               <Link href="/contact">Liên hệ</Link>
             </Nav.Item>
             <SearchBar />
