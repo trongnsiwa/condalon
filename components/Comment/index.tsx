bd4ffcdf66975c57629209470969ae3adc8f15ec
Si Trong <trongnsiwa79@gmail.com>
2022-02-27 10:06:18 +0700

    style: comment section size and add favicon

--- components/Comment/index.tsx
+++ components/Comment/index.tsx
@@ -2,43 +2,61 @@ import CommentForm from "@components/CommentForm";
 import React from "react";
 import styles from "./Comment.module.scss";
 
-function Comment ({comment, replies, activeComment, setActiveComment, addComment, parent = null}) {
-    const isReplying = activeComment && activeComment.id === comment.sys.id;
-    const createdAt = new Date(comment.sys.createdAt);
-    const reply = parent? parent : comment;
-    return (
-        <div className={styles.comment}>
-            <div className="comment-image">
-                <img src="https://tleliteracy.com/wp-content/uploads/2017/02/default-avatar.png"/>
-            </div>
-            <div className="commnet-rightSide">
-                <div className="comment-rightSide_content">
-                    <div className="comment-rightSide_content-author">{comment.fields.commentatorName}</div>
-                    <div className="comment-rightSide_content-createdAt">{createdAt.toLocaleDateString()} { createdAt.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</div>
-                </div>
-                <div className="comment-rightSide-text">{comment.fields.content}</div>
-                <div className="comment-rightSide-actions">
-                    <div className="comment-rightSide-action" onClick={() =>
-                        {
-                            setActiveComment({id: comment.sys.id});
-                        }
-                    }>
-                        Trả lời
-                    </div>
-                </div>
-                {isReplying && (
-                    <CommentForm submitLabel="Trả lời" handleSubmit={(text, name) => addComment(text, name, reply)} hasCancelButton={true} handleCancel={() => {
-                        setActiveComment(null);
-                      }} width="65rem"/>
-                )}
-                <div className="comment-rightSide-replies">
-                    {replies.length > 0 && replies?.map((reply) => (
-                        <Comment comment={reply} key={reply.sys.id} replies={[]} activeComment={activeComment} setActiveComment={setActiveComment} parent={comment.parent} addComment={addComment}/>
-                    ))}
-                </div>
-            </div>
+function Comment({ comment, replies, activeComment, setActiveComment, addComment, parent = null }) {
+  const isReplying = activeComment && activeComment.id === comment.sys.id;
+  const createdAt = new Date(comment.sys.createdAt);
+  const reply = parent ? parent : comment;
+  return (
+    <div className={styles.comment}>
+      <div className="comment-image">
+        <img src="https://tleliteracy.com/wp-content/uploads/2017/02/default-avatar.png" />
+      </div>
+      <div className="commnet-rightSide">
+        <div className="comment-rightSide_content">
+          <div className="comment-rightSide_content-author">{comment.fields.commentatorName}</div>
+          <div className="comment-rightSide_content-createdAt">
+            {createdAt.toLocaleDateString()}{" "}
+            {createdAt.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}
+          </div>
         </div>
-    );
+        <div className="comment-rightSide-text">{comment.fields.content}</div>
+        <div className="comment-rightSide-actions">
+          <div
+            className="comment-rightSide-action"
+            onClick={() => {
+              setActiveComment({ id: comment.sys.id });
+            }}
+          >
+            Trả lời
+          </div>
+        </div>
+        {isReplying && (
+          <CommentForm
+            submitLabel="Trả lời"
+            handleSubmit={(text, name) => addComment(text, name, reply)}
+            hasCancelButton={true}
+            handleCancel={() => {
+              setActiveComment(null);
+            }}
+          />
+        )}
+        <div className="comment-rightSide-replies">
+          {replies.length > 0 &&
+            replies?.map((reply) => (
+              <Comment
+                comment={reply}
+                key={reply.sys.id}
+                replies={[]}
+                activeComment={activeComment}
+                setActiveComment={setActiveComment}
+                parent={comment.parent}
+                addComment={addComment}
+              />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Comment;
--- components/CommentSection/index.tsx
+++ components/CommentSection/index.tsx
@@ -6,53 +6,63 @@ import { BlogApi } from "services/blog";
 import styles from "./Comments.module.scss";
 
 const api = new BlogApi();
-function CommentSection ({blog_id}) {
-    const [commentList, setCommentList] = useState<IComment[]>();
-    const [activeComment, setActiveComment] = useState(null);
-    const getComment = async () => {
-        const data = await api.getBlogComments(blog_id);
-        console.log(data);
-        setCommentList(data);
-    }
-    const addComment = async (text, name, parent) => {
-        var comment = await api.createComment(name, text, parent);
-        await api.handleBlogComment(comment, blog_id);
-        getComment();
-        setActiveComment(null);
-    }
-    const getReplies = (commentID) => {
-        return commentList?.filter(comment => comment.fields.parent?.sys.id == commentID).sort(
-            (a, b) =>  new Date(a.sys.createdAt).getTime() - new Date(b.sys.createdAt).getTime());
-    }
-    useEffect(() => {
-        getComment();
-      }, []);
-    const rootComments = commentList?.filter(comment => comment.fields.parent == null).sort(
-        (a, b) =>  new Date(b.sys.createdAt).getTime() - new Date(a.sys.createdAt).getTime());
-    return (
-        <div className={styles.comments}>
-            <div className="comments-form">
-                <div className="comments-form_title">
-                    <h4>Viết bình luận</h4>
-                </div>
-                <CommentForm submitLabel="Bình luận" handleSubmit={addComment} handleCancel={() => {
-                    setActiveComment(null);
-                }} width="85rem"/>
-            </div>
-            {rootComments?.length > 0 && (
-                <h3 className="comments-title">
-                Bình luận
-                </h3>
-            )}
-            <div className="comments-container">
-                {rootComments?.length > 0 && rootComments?.map((comment) => {
-                  return (
-                    <Comment comment={comment} key={comment.sys.id} replies={getReplies(comment.sys.id)} activeComment={activeComment} setActiveComment={setActiveComment} addComment={addComment}/>
-                  );
-                })}
-            </div>
+function CommentSection({ blog_id }) {
+  const [commentList, setCommentList] = useState<IComment[]>();
+  const [activeComment, setActiveComment] = useState(null);
+  const getComment = async () => {
+    const data = await api.getBlogComments(blog_id);
+    console.log(data);
+    setCommentList(data);
+  };
+  const addComment = async (text, name, parent) => {
+    var comment = await api.createComment(name, text, parent);
+    await api.handleBlogComment(comment, blog_id);
+    getComment();
+    setActiveComment(null);
+  };
+  const getReplies = (commentID) => {
+    return commentList
+      ?.filter((comment) => comment.fields.parent?.sys.id == commentID)
+      .sort((a, b) => new Date(a.sys.createdAt).getTime() - new Date(b.sys.createdAt).getTime());
+  };
+  useEffect(() => {
+    getComment();
+  }, []);
+  const rootComments = commentList
+    ?.filter((comment) => comment.fields.parent == null)
+    .sort((a, b) => new Date(b.sys.createdAt).getTime() - new Date(a.sys.createdAt).getTime());
+  return (
+    <div className={styles.comments}>
+      <div className="comments-form">
+        <div className="comments-form_title">
+          <h4>Viết bình luận</h4>
         </div>
-    );
+        <CommentForm
+          submitLabel="Bình luận"
+          handleSubmit={addComment}
+          handleCancel={() => {
+            setActiveComment(null);
+          }}
+        />
+      </div>
+      {rootComments?.length > 0 && <h4 className="comments-title">Bình luận</h4>}
+      <div className="comments-container">
+        {rootComments?.length > 0 &&
+          rootComments?.map((comment) => {
+            return (
+              <Comment
+                comment={comment}
+                key={comment.sys.id}
+                replies={getReplies(comment.sys.id)}
+                activeComment={activeComment}
+                setActiveComment={setActiveComment}
+                addComment={addComment}
+              />
+            );
+          })}
+      </div>
+    </div>
+  );
 }
 
 export default CommentSection;
