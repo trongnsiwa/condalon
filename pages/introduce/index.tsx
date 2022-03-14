f410012be6508e042cd72f8196e24d69b3d534c1
Si Trong <trongnsiwa79@gmail.com>
2022-03-14 15:26:20 +0700

    style: more resposive

--- pages/introduce/index.tsx
+++ pages/introduce/index.tsx
@@ -36,7 +36,7 @@ const Introduce = () => {
         {/* hero */}
         <div className="introduce-bgImg">
           <div className="introduce-bgImg_content">
-            <Fade direction="down">
+            <Fade direction="down" triggerOnce>
               <h1>Đôi Lời Về Dự Án</h1>
               <p>
                 “Life is full of beauty. Notice it. Notice the bumble bee, the small child, and the
@@ -48,7 +48,7 @@ const Introduce = () => {
         </div>
         <div className="introduce-preface">
           <div className="introduce-preface_left">
-            <Fade direction="left">
+            <Fade direction="left" triggerOnce>
               <img
                 src="https://images.unsplash.com/photo-1551304710-ece312832e87?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=436&q=80"
                 alt=""
@@ -56,7 +56,7 @@ const Introduce = () => {
             </Fade>
           </div>
           <div className="introduce-preface_right">
-            <Fade direction="right">
+            <Fade direction="right" triggerOnce>
               <h2>{getFields("preface")?.title}</h2>
               <p>{getFields("preface")?.description}</p>
             </Fade>
@@ -64,7 +64,7 @@ const Introduce = () => {
         </div>
         <div className="introduce-goal">
           <div className="introduce-goal_content">
-            <Fade direction="right">
+            <Fade direction="right" triggerOnce>
               <h2>{getFields("goal")?.title}</h2>
               <p>{getFields("goal")?.description}</p>
             </Fade>
@@ -72,7 +72,7 @@ const Introduce = () => {
         </div>
         <div className="introduce-team">
           <div className="introduce-team_left">
-            <Fade direction="right">
+            <Fade direction="right" triggerOnce>
               <h2>{getFields("team")?.title}</h2>
               <p>{getFields("team")?.description}</p>
               <Link href="/about-us">
@@ -81,7 +81,7 @@ const Introduce = () => {
             </Fade>
           </div>
           <div className="introduce-team_right">
-            <Fade direction="left">
+            <Fade direction="left" triggerOnce>
               <img
                 src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
                 alt=""
